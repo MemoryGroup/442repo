f165129bcfb17ce6adad4e6d10aef4f974ceb2f0
youngkih <youngkihong92@gmail.com>
2016-09-17 21:15:22 -0400

    Memory_Project

---
+++ Memory_Project/app/src/main/java/com/example/youngki/memory_project/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.youngki.memory_project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onStartButtonClicked(View v){
+        Button startButton = (Button) v;
+        Intent startWindowOpener = new Intent(this,showRandomNumber.class);
+        startActivity(startWindowOpener);
+    }
+    public void onHelpButtonClicked(View v){
+        Button helpButton = (Button) v;
+        Intent helpWindowOpener = new Intent(this,showHelp.class);
+        startActivity(helpWindowOpener);
+
+
+    }
+}
