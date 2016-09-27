a4fe8b9b86d2878ee7c3d4cff03613c370a6d03a
Amy Pritchard <amypritc@buffalo.edu>
2016-09-26 20:51:03 -0400

    added test button and layout for testing

---
+++ app/src/main/java/com/example/youngki/memory_project/Test.java
@@ -0,0 +1,13 @@
+package com.example.youngki.memory_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
