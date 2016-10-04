49d6eff182aac917823fada236eaed5c2bfe7d3c
Amy Pritchard <amypritc@buffalo.edu>
2016-10-03 22:44:23 -0400

    added a progress bar and show results action
    -progress bar added, shows a bar across top of how far you are in the
    test
    -currently, only button "1" will make test go further
    -after the last 'question' has been answered jumps to results screen,
    currently with generic text, will eventually show the correct matchings
    with whether it was answered correct or incorrect

--- app/src/main/java/com/example/youngki/memory_project/showTest.java
+++ app/src/main/java/com/example/youngki/memory_project/showTest.java
@@ -1,31 +1,59 @@
 package com.example.youngki.memory_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import java.util.HashMap;
 
 
 public class showTest extends AppCompatActivity {
 
-    //HashMap<String, Integer> memoryMap = new HashMap<>(26);
-    //String[] keys;
+    String[] keys = {"A", "B", "C", "D", "E", "F"};
+    Integer[] values = {0, 1, 2, 3, 4, 5};
+    String showThis = this.keys[0];
+    int count = 0;
+    HashMap<String, Integer> correct = new HashMap<>();
+    //String [] correct;
 
-    //public void setMemoryMap(HashMap<String, Integer> memoryMap) {
-    //    memoryMap.put("A", 1);
-    //    memoryMap.put("B", 0);
-    //    memoryMap.put("C", 5);
-    //    memoryMap.put("D", 7);
-    //    this.memoryMap = memoryMap;
-    //    memoryMap.keySet().toArray(this.keys);
-    //}
+    public void showResultsNow(){
+        Intent resultsWindowOpener = new Intent(this,showResults.class);
+        //resultsWindowOpener.putExtra("correct", correct);
+        startActivity(resultsWindowOpener);
+    }
+
+    public void getNextLetter(){
+        this.count = this.count + 1;
+        if (count < keys.length){
+            this.showThis = this.keys[this.count];
+            TextView tv = (TextView) findViewById(R.id.textView3);
+            tv.setText(showThis);
+            ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
+            pb.incrementProgressBy(1);
+        }
+        else{
+            showResultsNow();
+        }
+    }
+
+    public void onOneClicked(View view){
+        if (values[count] == 1){
+            correct.put(keys[count], values[count]);
+        }
+        getNextLetter();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        //setMemoryMap(memoryMap);
-        //TextView tv = (TextView) findViewById(R.id.textView3);
-        //tv.setText(this.keys[0]);
+    //    setMemoryMap(memoryMap);
+        TextView tv = (TextView) findViewById(R.id.textView3);
+    //    this.showThis = this.keys[3];
+        tv.setText(this.showThis);
+        ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb.setMax(keys.length);
     }
 }
