385f122804ff5e5eba322997ee072c21c8f5f651
youngkih <youngkihong92@gmail.com>
2016-10-06 10:04:15 -0400

    change Menu's style and make Help button

--- app/src/main/java/com/example/youngki/memory_project/MainActivity.java
+++ app/src/main/java/com/example/youngki/memory_project/MainActivity.java
@@ -5,13 +5,19 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+
+
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+    private AlphaAnimation buttonClicked = new AlphaAnimation(1F, 0.8F);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +30,35 @@ public class MainActivity extends AppCompatActivity {
         Animation anim = AnimationUtils.loadAnimation(this,R.anim.fade_in);
         textView.setAnimation(anim);
 
+        //Setting fonts of buttons
+        button = (Button)findViewById(R.id.button);
+        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+
+        button = (Button)findViewById(R.id.button2);
+        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+
+        button = (Button)findViewById(R.id.button3);
+        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+
+        button = (Button)findViewById(R.id.button4);
+        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+
+
+
     }
     public void onStartButtonClicked(View v){
-        Button startButton = (Button) v;
+        v.startAnimation(buttonClicked);
         Intent startWindowOpener = new Intent(this,createMap.class);
         startActivity(startWindowOpener);
     }
     public void onHelpButtonClicked(View v){
-        Button helpButton = (Button) v;
+        v.startAnimation(buttonClicked);
         Intent helpWindowOpener = new Intent(this,showHelp.class);
         startActivity(helpWindowOpener);
     }
 
     public void onTestButtonClicked(View v){
+        v.startAnimation(buttonClicked);
         Intent testWindowOpener = new Intent(this,showTest.class);
         startActivity(testWindowOpener);
     }
