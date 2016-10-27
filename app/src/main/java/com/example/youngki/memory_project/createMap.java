8b6f93ce951ad066d0a3ad95261fb817824899c0
youngkih <youngkihong92@gmail.com>
2016-10-27 12:55:18 -0400

    handwriting function added

--- app/src/main/java/com/example/youngki/memory_project/createMap.java
+++ app/src/main/java/com/example/youngki/memory_project/createMap.java
@@ -1,18 +1,18 @@
 package com.example.youngki.memory_project;
 
-        import android.content.Intent;
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-        import java.util.ArrayList;
-        import java.util.Arrays;
-        import java.util.Collections;
-        import java.util.HashMap;
-        import java.util.Random;
-        import android.graphics.Typeface;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
+import android.graphics.Typeface;
 
 
 
