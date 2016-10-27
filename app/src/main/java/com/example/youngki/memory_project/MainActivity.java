29af999b96a44b51d33bd3cf108d2b8f5b1a30ea
moejuran <majuran@buffalo.edu>
2016-10-27 10:00:29 -0400

    made numbers to voice

--- app/src/main/java/com/example/youngki/memory_project/MainActivity.java
+++ app/src/main/java/com/example/youngki/memory_project/MainActivity.java
@@ -11,56 +11,50 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
-
-
-
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
-    private AlphaAnimation buttonClicked = new AlphaAnimation(1F, 0.8F);
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private Button button;
+  private AlphaAnimation buttonClicked = new AlphaAnimation(1F, 0.8F);
 
-        TextView textView = (TextView)findViewById(R.id.textView2);
-        Typeface type = Typeface.createFromAsset(getAssets(),"fonts/orange juice 2.0.ttf");
-        textView.setTypeface(type);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    TextView textView = (TextView) findViewById(R.id.textView2);
+    Typeface type = Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf");
+    textView.setTypeface(type);
+    Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+    textView.setAnimation(anim);
+    //Setting fonts of buttons
+    button = (Button) findViewById(R.id.button);
+    button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+    button = (Button) findViewById(R.id.button2);
+    button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+    button = (Button) findViewById(R.id.button3);
+    button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+    button = (Button) findViewById(R.id.button4);
+    button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+    findViewById(R.id.numberButton).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        startActivity(new Intent(MainActivity.this,NumberButtons.class));
+      }
+    });
+  }
 
-        Animation anim = AnimationUtils.loadAnimation(this,R.anim.fade_in);
-        textView.setAnimation(anim);
+  public void onStartButtonClicked(View v) {
+    v.startAnimation(buttonClicked);
+    Intent startWindowOpener = new Intent(this, createMap.class);
+    startActivity(startWindowOpener);
+  }
 
-        //Setting fonts of buttons
-        button = (Button)findViewById(R.id.button);
-        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
+  public void onHelpButtonClicked(View v) {
+    v.startAnimation(buttonClicked);
+    Intent helpWindowOpener = new Intent(this, showHelp.class);
+    startActivity(helpWindowOpener);
+  }
 
-        button = (Button)findViewById(R.id.button2);
-        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
-
-        button = (Button)findViewById(R.id.button3);
-        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
-
-        button = (Button)findViewById(R.id.button4);
-        button.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/orange juice 2.0.ttf"));
-
-
-
-    }
-    public void onStartButtonClicked(View v){
-        v.startAnimation(buttonClicked);
-        Intent startWindowOpener = new Intent(this,createMap.class);
-        startActivity(startWindowOpener);
-    }
-    public void onHelpButtonClicked(View v){
-        v.startAnimation(buttonClicked);
-        Intent helpWindowOpener = new Intent(this,showHelp.class);
-        startActivity(helpWindowOpener);
-    }
-
-    public void onTestButtonClicked(View v){
-        v.startAnimation(buttonClicked);
-        Intent testWindowOpener = new Intent(this,showTest.class);
-        startActivity(testWindowOpener);
-    }
-
+  public void onTestButtonClicked(View v) {
+    v.startAnimation(buttonClicked);
+    Intent testWindowOpener = new Intent(this, showTest.class);
+    startActivity(testWindowOpener);
+  }
 }
