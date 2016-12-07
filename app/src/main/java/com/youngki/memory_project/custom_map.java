4f046300bc0b8b0e6c2224a611d7c508f029c22d
Amy Pritchard <amypritc@buffalo.edu>
2016-12-06 20:49:19 -0500

    merge in fixed custom map

--- app/src/main/java/com/youngki/memory_project/custom_map.java
+++ app/src/main/java/com/youngki/memory_project/custom_map.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -37,7 +36,7 @@ public class custom_map extends AppCompatActivity {
             clearMap();
             if (checkLetters(letters)){
                 saveMap(letters, numbers);
-                Intent windowOpener = new Intent(this,ViewMapActivity.class);
+                Intent windowOpener = new Intent(this,progress.class);
                 startActivity(windowOpener);
             }
             else{
@@ -54,7 +53,7 @@ public class custom_map extends AppCompatActivity {
     public Boolean checkLetters(String s){
         for (int i = 0; i < s.length(); i++){
             char c = s.charAt(i);
-            String str = Character.toString(c);
+            String str = Character.toString(c).toUpperCase();
             if (alpha.contains(str)){
                 if (!alphabet.get(str)){
                     alphabet.put(str, Boolean.TRUE);
@@ -72,7 +71,7 @@ public class custom_map extends AppCompatActivity {
 
     public void saveMap(String letters, String numbers){
         for (int i = 0; i < letters.length(); i++){
-            String let = Character.toString(letters.charAt(i));
+            String let = Character.toString(letters.charAt(i)).toUpperCase();
             int num = Integer.parseInt(Character.toString(numbers.charAt(i)));
             memMap.put(let,num);
         }
